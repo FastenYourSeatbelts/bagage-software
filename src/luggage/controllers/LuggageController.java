0c34522328737e4136ef023965fd7bc7012d296e
Alexander <Alexander@145.109.142.251>
2014-11-19 13:24:35 +0100

    Added luggage screen controls, made reset almost work

--- src/luggage/controllers/LuggageController.java
+++ src/luggage/controllers/LuggageController.java
@@ -25,18 +25,25 @@
 package luggage.controllers;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import luggage.database.models.CustomerModel;
 import luggage.database.models.LuggageModel;
 import luggage.database.models.Model;
+import luggage.helpers.StageHelper;
 
 /**
  * LuggageController
@@ -46,92 +53,178 @@ import luggage.database.models.Model;
  * @package luggage.controllers
  * @author Tijme Gommers
  */
-public class LuggageController extends BaseController  implements Initializable {
+public class LuggageController extends BaseController implements Initializable {
 
     @FXML
-    private TableView luggageTableView;
-    
+    private TableView listTableView;
+
     @FXML
-    private TableColumn tableViewId;
-    
+    private TableColumn listTableViewId;
+
     @FXML
-    private TableColumn tableViewStatus;
-    
+    private TableColumn listTableViewStatus;
+
     @FXML
-    private TableColumn tableViewLocation;
-    
+    private TableColumn listTableViewLocation;
+
     @FXML
-    private TableColumn tableViewTags;
-    
+    private TableColumn listTableViewTags;
+
     @FXML
-    private TableColumn tableViewDate;
-    
+    private TableColumn listTableViewDate;
+
     @FXML
-    private TextField searchbox;
-    
+    private TextField listSearchField;
+
     @FXML
-    protected void onKeyReleased()  {
-        
-        String[] keywords = searchbox.getText().split("\\s+");
-        
+    private Button newAdd;
+
+    @FXML
+    private Button newReset;
+
+    @FXML
+    private Button newCancel;
+
+    @FXML
+    private TextField addFirstname;
+
+    @FXML
+    private TextField addPrefix;
+
+    @FXML
+    private TextField addLastname;
+
+    @FXML
+    private TextField addTags;
+
+    @FXML
+    private TextField addLocation;
+
+    @FXML
+    private TextField addNotes;
+
+    @FXML
+    private DatePicker addDate;
+
+    @FXML
+    private ChoiceBox addStatus;
+
+    private ObservableList<LuggageModel> listData = FXCollections.observableArrayList();
+
+    /**
+     * Called on controller start
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // List
+        if (listTableView != null) {
+            listResetTableView("", new String[0]);
+        }
+
+        // Add
+        if (addStatus != null) {
+            setAddChoiceBoxes();
+        }
+
+    }
+
+    public void setAddChoiceBoxes() {
+        addStatus.setItems(FXCollections.observableArrayList(
+                "",
+                "Missing",
+                "Found",
+                "Resolved"
+        ));
+    }
+
+    @FXML
+    protected void listOnSearch() {
+
+        String[] keywords = listSearchField.getText().split("\\s+");
+
         String[] params = new String[3 * keywords.length];
         boolean firstColumn = true;
         String query = "";
-        
-        for(int i = 0; i < keywords.length; i ++)
-        {
-            if(firstColumn) {
+
+        for (int i = 0; i < keywords.length; i++) {
+            if (firstColumn) {
                 params[0 + i] = "%" + keywords[i] + "%";
                 query += "id LIKE ?";
             } else {
                 params[0 + i] = "%" + keywords[i] + "%";
                 query += " OR id LIKE ?";
             }
-            
+
             params[1 + i] = "%" + keywords[i] + "%";
             query += " OR tags LIKE ?";
- 
+
             params[2 + i] = "%" + keywords[i] + "%";
             query += " OR datetime LIKE ?";
-            
+
             firstColumn = false;
         }
-        
-        resetTableView(query, params);
-    }
-    
-    private ObservableList<LuggageModel> data = FXCollections.observableArrayList();   
 
-    
-    /**
-     * Called on controller start
-     * 
-     * @param url
-     * @param rb 
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        resetTableView("", new String[0]);
+        listResetTableView(query, params);
     }
-    
-    public void resetTableView(String where, String... params) {
+
+    @FXML
+    public void listNew() {
+        StageHelper.addStage("luggage/add", this, false, true);
+    }
+
+    public void listResetTableView(String where, String... params) {
         LuggageModel luggage = new LuggageModel();
         List<Model> allLuggage = luggage.findAll(where, params);
-        
-        data = FXCollections.observableArrayList(); 
-        for(int i = 0; i < allLuggage.size(); i ++){
-            LuggageModel luggage2 = (LuggageModel) allLuggage.get(i);
-            data.add(luggage2);
+
+        listData = FXCollections.observableArrayList();
+        for (int i = 0; i < allLuggage.size(); i++) {
+            LuggageModel luggage2 = (LuggageModel) allLuggage;
+            listData.add(luggage2);
         }
-        
-        
-        tableViewId.setCellValueFactory(new PropertyValueFactory("id"));
-        tableViewStatus.setCellValueFactory(new PropertyValueFactory("status"));
-        tableViewLocation.setCellValueFactory(new PropertyValueFactory("locationName"));
-        tableViewTags.setCellValueFactory(new PropertyValueFactory("tags"));
-        tableViewDate.setCellValueFactory(new PropertyValueFactory("datetime"));
-                    
-        luggageTableView.setItems(data);
+
+        listTableViewId.setCellValueFactory(new PropertyValueFactory("id"));
+        listTableViewStatus.setCellValueFactory(new PropertyValueFactory("status"));
+        listTableViewLocation.setCellValueFactory(new PropertyValueFactory("locationName"));
+        listTableViewTags.setCellValueFactory(new PropertyValueFactory("tags"));
+        listTableViewDate.setCellValueFactory(new PropertyValueFactory("datetime"));
+
+        listTableView.setItems(listData);
     }
-    
+
+    public void newCancel() {
+        Stage addStage = (Stage) newCancel.getScene().getWindow();
+        StageHelper.closeStage(addStage);
+    }
+
+    public void newReset() {
+        addFirstname.setText("");
+        addPrefix.setText("");
+        addLastname.setText("");
+        addTags.setText("");
+        addLocation.setText("");
+        addNotes.setText("");
+        addStatus.setValue("");
+    }
+
+    public void newSave() {
+        if (addStatus.getSelectionModel().getSelectedItem() == null) {
+            return;
+        }
+
+        LuggageModel luggage = new LuggageModel();
+        luggage.setStatus(addStatus.getSelectionModel().getSelectedItem().toString());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String dateString = sdf.format(addDate.getValue());
+        luggage.setDatetime(dateString);
+        luggage.setTags(addTags.getText());
+        luggage.setNotes(addNotes.getText());
+        luggage.save();
+        LuggageController luggageController = (LuggageController) StageHelper.callbackController;
+        luggageController.listOnSearch();
+
+        newCancel();
+    }
 }
