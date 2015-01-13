a34c7845c8d2c6481423e57b840bc1e0714d753c
Tijme <tijme@ucfirst.nl>
2015-01-13 15:09:37 +0100

    Kleren zooi

--- src/luggage/controllers/LuggageGraphController.java
+++ src/luggage/controllers/LuggageGraphController.java
@@ -45,8 +45,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
